cc6acba4878b2bd3815832ebe48c1c0d6105702d
Jyotsna Jose <jyotsnamaria@gmail.com>
2021-11-25 20:23:33 -0600

    Added thunk and logger middleware.
    Data from DB accessed by the API call.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,3 +1,5 @@
+//thunk action creator that gets the data from the database
+//and then dispatches actions to the store to set the three pieces of state we have in our store
 import { getInitialData } from "../utils/api";
 import { setAuthedUser } from "./authedUser";
 import { receiveTweets } from "./tweets";
@@ -6,7 +8,6 @@ import { receiveUsers } from "./users";
 export const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
-  //uses thunk middleware.. async API call
   return (dispatch) => {
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
